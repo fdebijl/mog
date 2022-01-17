6a1ce86407c44b77d49cfbf14bd6bb9aa49e45ef
Floris de Bijl <floris@feedbackfruits.com>
2022-01-17 20:04:15 +0100

    feat: allow find options to be passed through to list operation

--- src/domain/type/index.ts
+++ src/domain/type/index.ts
@@ -1,6 +1,7 @@
 export * from './CountOptions';
 export * from './GetOptions';
 export * from './InsertOptions';
+export * from './ListOptions';
 export * from './MogOptions';
 export * from './Operation';
 export * from './OperationOptions';
